9cda41cbab4effe5065d25389978d91ccc555db9
gabo <gabo.rage@gmail.com>
2020-01-30 16:53:18 -0600

    Initial Commit

---
+++ DemoFramework/DemoFramework.h
@@ -0,0 +1,19 @@
+//
+//  DemoFramework.h
+//  DemoFramework
+//
+//  Created by Gabriel Rosales on 1/30/20.
+//  Copyright © 2020 Iguama Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoFramework.
+FOUNDATION_EXPORT double DemoFrameworkVersionNumber;
+
+//! Project version string for DemoFramework.
+FOUNDATION_EXPORT const unsigned char DemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoFramework/PublicHeader.h>
+
+
